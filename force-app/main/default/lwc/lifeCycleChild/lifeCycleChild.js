a25a9e15a4859e90c6f75063eeac2d1ac5500c39
abhi070301 <abhishekbairwa373@gmail.com>
2024-04-04 07:48:51 +0530

    4Ap

---
+++ force-app/main/default/lwc/lifeCycleChild/lifeCycleChild.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class LifeCycleChild extends LightningElement {
+    constructor(){
+        super()
+        console.log("Child Constructor Called...")
+    }
+    connectedCallback(){
+        console.log("Child connectedCallback Called...")
+        throw new Error('Loading of child component failed')
+    }
+    renderedCallback(){
+        console.log("Child renderedCallback Called...")
+    }
+    disconnectedCallback(){
+        alert('Child disconnectedCallback called...')
+    }
+}
---
+++ force-app/main/default/lwc/p2cCarouselChildComponent/p2cCarouselChildComponent.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class P2cCarouselChildComponent extends LightningElement {
+    @api carouselDetail
+}
