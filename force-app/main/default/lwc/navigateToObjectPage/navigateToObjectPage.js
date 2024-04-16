c2f635c8054bcf13b1725551753cc906afdbec29
abhi070301 <abhishekbairwa373@gmail.com>
2024-04-16 07:58:50 +0530

    NewRecordNavigation

---
+++ force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation'
+export default class NavigateToObjectPage extends NavigationMixin(LightningElement) {
+    navigateToNewRecord(){
+        this[NavigationMixin.Navigate]({
+            type:"standard__objectPage",
+            attributes:{
+                objectApiName:'Contact',
+                actionName: 'new'
+            }
+        })
+    }
+}
